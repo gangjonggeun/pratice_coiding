fde7cc5c7c7207103160921c5962b0541f75e7eb
강종근 <80893900+gangjonggeun@users.noreply.github.com>
2023-08-05 19:18:33 +0900

    동적 프로그래밍 연습

---
+++ dynamicPrograming.java
@@ -0,0 +1,13 @@
+package ploblem;
+
+public class dynamicPrograming {
+	public static void main(String[] args) {
+			int s[] = {6,9,13,14,20,21,22,23,30,49,55};
+			
+			int n = s.length; // 배열의 수 
+			int m = 110; // 만들 수
+			
+			
+			
+	}
+}
